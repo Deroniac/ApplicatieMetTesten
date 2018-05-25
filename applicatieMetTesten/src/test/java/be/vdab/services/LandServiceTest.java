8f1fc3f49770277a9aca434d8aa7ea2e3d569cc4
Deroniac <33629048+Deroniac@users.noreply.github.com>
2018-05-25 15:24:23 +0200

    Einde Cursus

--- applicatieMetTesten/src/test/java/be/vdab/services/LandServiceTest.java
+++ applicatieMetTesten/src/test/java/be/vdab/services/LandServiceTest.java
@@ -4,18 +4,30 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import be.vdab.entities.Land;
 import be.vdab.repositories.LandRepository;
 import be.vdab.repositories.LandRepositoryStub;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LandServiceTest {
+	@Mock
 	private LandRepository landRepository;
 	private LandService landService;
 	@Before public void before() {
+		when(landRepository.findOppervlakteAlleLanden()).thenReturn(20);
+		when(landRepository.read("B")).thenReturn(new Land("B", 5));
 		landRepository = new LandRepositoryStub();
 		landService = new LandService(landRepository);
 	}
 	@Test public void findVerhoudingOppervlakteLandTovOppervlakteAlleLanden() {
 		assertEquals(0, BigDecimal.valueOf(0.25).compareTo(
 				landService.findVerhoudingOppervlakteLandTovOppervlakteAlleLanden("B")));
+		verify(landRepository).findOppervlakteAlleLanden();
+		verify(landRepository).read("B");
 	}
 }
